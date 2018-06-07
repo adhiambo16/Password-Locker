93b1f5eaa1f243771e7d8895c7d5d392224c9c5e
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-07 15:01:37 +0300

    Added a class

---
+++ credential.py
@@ -0,0 +1,11 @@
+class Credential:
+    # '''
+    # Class that generates new instances of credential.
+    # '''
+    # def __init__(self,accountname,password,email):
+    #
+    #     #docstring removed
+    #
+    #     self.accountname = accountname
+    #     self.password = password
+    #     self.email = email
