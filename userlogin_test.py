10ad7f18682df48118c07d9b73bb27f7c073917d
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-08 16:12:06 +0300

    added a subclass

--- userlogin_test.py
+++ userlogin_test.py
@@ -1,11 +1,11 @@
 import unittest # Importing the unittest module
 from  Userlogin import User # Importing the contact class
-#
-# class TestContact(unittest.TestCase):
-#
-#     '''
-#     Test class that defines test cases for the contact class behaviours.
-#
-#     Args:
-#         unittest.TestCase: TestCase class that helps in creating test cases
-#     '''
+
+class TestContact(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the contact class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
