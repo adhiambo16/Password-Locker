65a6d233319469b3c50cf8c0cd0455587996ab0e
Oluoch Lydia <adhiambolydia96@gmail.com>
2018-06-08 16:11:22 +0300

    imported a module

--- userlogin_test.py
+++ userlogin_test.py
@@ -1,5 +1,5 @@
-# import unittest # Importing the unittest module
-# from  Userlogin import User # Importing the contact class
+import unittest # Importing the unittest module
+from  Userlogin import User # Importing the contact class
 #
 # class TestContact(unittest.TestCase):
 #
